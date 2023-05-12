a92f6df273386c9b88abd173ed30412fd6e28be4
krishmurarka <murarka.krish@gmail.com>
2023-05-12 16:36:21 +0530

    backend setup and database connection

---
+++ src/main/java/com/example/washingmachinebackend/WashingMachineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.washingmachinebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashingMachineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashingMachineBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/washingmachinebackend/WashingMachineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.washingmachinebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashingMachineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
